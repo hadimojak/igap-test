66b2e5a9d2d9289eac36ac646dc1dc9f808fcf69
hadi arbabi <hadimojak@gmail.com>
2025-05-14 02:14:54 +0330

    fix

--- sdd-app/dist/main.js
+++ sdd-app/dist/main.js
@@ -3,10 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const dotenv = require("dotenv");
+const consola_1 = require("consola");
 async function bootstrap() {
     dotenv.config();
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     await app.listen(process.env.PORT);
 }
-bootstrap();
+bootstrap()
+    .then(() => {
+    consola_1.consola.success('server succesfully started!');
+})
+    .catch((err) => console.log(err));
 //# sourceMappingURL=main.js.map
--- sdd-app/src/main.ts
+++ sdd-app/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { consola } from 'consola';
 
 async function bootstrap() {
   dotenv.config();
@@ -8,4 +9,8 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT as string);
 }
-bootstrap();
+bootstrap()
+  .then(() => {
+    consola.success('server succesfully started!');
+  })
+  .catch((err) => console.log(err));
